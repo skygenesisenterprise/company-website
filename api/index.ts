d07fa2d562125d3fe6527f390326c0d2f15205a2
Liam Von Astoria <hello@liamvonastoria.net>
2025-10-08 07:57:39 +0200

    Update tsconfig.json file & Index Backend

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 3002;
+const port = 3001;
 
 app.use(bodyParser.json());
 
