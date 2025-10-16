1a9c93eca0a2351984ac410ebce6ae7cf37ad237
Liam Von Astoria <hello@liamvonastoria.net>
2025-10-16 09:30:23 +0200

    Update Interface client

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@ import { HeroSection, AboutSection, ProductsSection, GovernanceSection, Industri
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white text-slate-900 flex flex-col items-center justify-center">
-      <div className="w-full max-w-7xl">
-        <HeroSection />
-        <AboutSection />
-        <ProductsSection />
-        <GovernanceSection />
-        <IndustriesSection />
-        <BlogSection />
-        <VisionSection />
-      </div>
+    <div>
+      <HeroSection />
+      <AboutSection />
+      <ProductsSection />
+      <GovernanceSection />
+      <IndustriesSection />
+      <BlogSection />
+      <VisionSection />
     </div>
   );
 }
